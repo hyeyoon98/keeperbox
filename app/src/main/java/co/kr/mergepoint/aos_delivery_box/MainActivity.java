550218211e82b3816eed5fb4a98abb6744f537bd
keeperbox <ymcacom665@gmail.com>
2020-10-06 17:46:52 +0900

    first commit

---
+++ app/src/main/java/co/kr/mergepoint/aos_delivery_box/MainActivity.java
@@ -0,0 +1,14 @@
+package co.kr.mergepoint.aos_delivery_box;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
